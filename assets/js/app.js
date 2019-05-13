92454ef3d9d01fadb3b6cbdc15be3a21cd50d89f
mugimugitkr <mugimugithirty@gmail.com>
2019-05-13 20:07:34 +0900

    no message

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
+// 送信者が自分かどうか保存する変数
+// true:自分
+// false:相手
+let isMySelf = true;
+
 // 送信ボタンの取得
 let sendBtn = document.getElementById('sendBtn');
 
@@ -9,11 +14,25 @@ sendBtn.addEventListener('click', function() {
   // 入力値を取得
   let messageText = inputMessage.value;
 
+  // 入力値が空かチェックする
+  if (messageText == '') {
+    // 入力値が空の場合、処理を中断する
+    return;
+  }
+
   // divを作成
   let messageBox = document.createElement('div');
 
-  // 「box-right」というクラスをdivに追加
-  messageBox.classList.add('box-right');
+  // 誰が送信者かチェック
+  if (isMySelf) {
+    // 自分が送信者の場合
+    // 「box-right」というクラスをdivに追加
+    messageBox.classList.add('box-right');
+  } else {
+    // 相手が送信者の場合
+    // 「box-left」というクラスをdivに追加
+    messageBox.classList.add('box-left');
+  }
 
   // pタグを作成
   let message = document.createElement('p');
@@ -24,8 +43,15 @@ sendBtn.addEventListener('click', function() {
   // pタグに「message-box」というクラスを追加
   message.classList.add('message-box');
 
-  // 「green」というクラスをpに追加
-  message.classList.add('green');
+  if (isMySelf) {
+    // 自分が送信者の場合
+    // 「green」というクラスをpに追加
+    message.classList.add('green');
+  } else {
+    // 相手が送信者の場合
+    // 「white」というクラスをpに追加
+    message.classList.add('white');
+  }
 
   // divにpタグを追加する
   messageBox.appendChild(message);
@@ -39,4 +65,14 @@ sendBtn.addEventListener('click', function() {
   // 入力欄の入力値をリセットする
   inputMessage.value = '';
 
+  // 送信者を変更する
+  if (isMySelf) {
+    // 自分が送信者の場合
+    // 次の送信者を相手にする
+    isMySelf = false;
+  } else {
+    // 相手が送信者の場合
+    // 次の送信者を自分にする
+    isMySelf = true;
+  }
 })
